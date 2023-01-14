a3ffbe8fa922710d4ac618bf84552b95e8559cb9
surabhi patel <surabhipatel2101@gmail.com>
2023-01-14 13:21:14 +0530

    updated ruby to 2.7.2 and rails to 6.1.7

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  #get 'pages/index'
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pages
   resources :contacts
   root to: 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
